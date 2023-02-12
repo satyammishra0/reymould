06d373b86cb8b4887fe961ddc3230c134b24a754
satyam mishra <satyam.growupnext@gmail.com>
2023-02-12 22:56:00 +0530

    new header css

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,17 @@ document.addEventListener("click", (e) => {
   accordionContent.classList.toggle("show");
   e.target.innerText = e.target.innerText === "+" ? "-" : "+";
 });
+
+// ---------------------
+// Header Transformation
+// ---------------------
+
+const mobileNav = document.querySelector(".mobile-navbar-btn");
+
+const header = document.querySelector(".header");
+
+function toggleMobileNav() {
+  header.classList.toggle("active");
+}
+
+mobileNav.addEventListener("click", () => toggleMobileNav());
